30268e7fad137550de6cd2c8bfefe5c4f312cf2e
xu756 <756334744@qq.com>
2022-06-29 23:16:29 +0800

    init

--- leetcode/editor/cn/[1]两数之和.java
+++ leetcode/editor/cn/[1]两数之和.java
@@ -46,16 +46,14 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-        Map<Integer, Integer> map = new HashMap<>();    // 哈希表
-        for (int i = 0; i < nums.length; i++) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
             int complement = target - nums[i];
-            if (map.containsKey(complement)) {
+            if(map.containsKey(complement)){
                 return new int[]{map.get(complement), i};
             }
             map.put(nums[i], i);
         }
-        throw new IllegalArgumentException("No two sum solution");
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
